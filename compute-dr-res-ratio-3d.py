2f6ed1fbaa31625ac0e8786e704f6597a8366e17
Shao Chun Lee <shaochunlee@Shaos-MacBook-Pro.local>
2022-12-06 21:55:12 -0600

    add mass weight

--- compute-dr-res-ratio-3d.py
+++ compute-dr-res-ratio-3d.py
@@ -5,6 +5,7 @@ from ZLabPlot import ZLabPlot
 
 mass_map = {'N':14.0067,'C':12.011,'O':15.9994,'S':15.9994,'H':1.00797}
 res_name_map = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','GLY':'G','HIS':'H','ILE':'I','LEU':'L','LYS':'K','MET':'M','PHE':'F','PRO':'P','SER':'S','THR':'T','TRP':'W','TYR':'Y','VAL':'V'}
+res_mass_map = {'A': 71.03711,'R':156.10111,'N':114.04293,'D':115.02694,'C':103.00919,'E':129.04259,'Q':128.05858,'G':57.02146,'H':137.5891,'I':113.08406,'L':113.08406,'K':128.09496,'M':131.04049,'F':147.06841,'P':97.05276,'S':87.03203,'T':101.04768,'W':186.07931,'Y':163.06333,'V':99.06841}
 
 def read_xyz(xyzfile):
     #argument
@@ -56,14 +57,19 @@ for i in range(len(folder)): folder[i] = folder[i].replace('./', "")
 folder.remove('wild')
 cutoff = 100
 window = 1
+sufix = ""
+mass_weight_fg = False
 res_cutoff = float(sys.argv[1])
+if '-mass' in sys.argv[2:]:
+    mass_weight_fg = True
+    sufix += '-massweighted'
 
 coord_wt, element_wt = read_xyz('WT/md-traj-kabsch-com.xyz')
 coord_wt = coord_wt[cutoff:]
 dr_wt = compute_dr_time_avg(coord_wt, 'all', window)
 r_wt = compute_r_time_avg(coord_wt, 'all')
 
-fout = open("res-com-msd-ratio-range-3d=%.2f.txt"%(res_cutoff), 'w')
+fout = open("res-com-msd-ratio-range-3d=%.2f%s.txt"%(res_cutoff, sufix), 'w')
 fout.write("mutation %12s %12s %12s %6s\n"%('dr_mu','dr_wt','dr_mu/dr_wt', 'n_res'))
 
 for i in range(len(folder)):
@@ -74,22 +80,32 @@ for i in range(len(folder)):
         print(i, folder[i])
         dr_mu_tmp = 1.0
         dr_wt_tmp = 1.0
-    elif ord('Z') >= ord(folder[i][-1]) >= ord('A'):
-        target_res_index = int(folder[i][1:-1]) - 1
-        res_to_target_res_distance_wt = np.sum((r_wt - r_wt[target_res_index])**2, axis = 1)**0.5
-        res_indices_wt = np.where(res_to_target_res_distance_wt <= res_cutoff)[0]
-        r_mu = compute_r_time_avg(coord_mu, 'all')
-        res_indices_mu = res_indices_wt
-        print(i, folder[i], list(res_indices_mu))
+        res_indices_mu = []
     else:
-        target_res_index = int(folder[i][1:]) - 1
-        res_to_target_res_distance_wt = np.sum((r_wt - r_wt[target_res_index])**2, axis = 1)**0.5
-        res_indices_wt = np.where(res_to_target_res_distance_wt <= res_cutoff)[0]
-        res_indices_wt = np.delete(res_indices_wt, np.where(res_indices_wt == target_res_index)[0])
-        res_indices_mu = np.where(res_indices_wt > target_res_index, res_indices_wt-1, res_indices_wt)
-        print(i, folder[i], list(res_indices_wt), list(res_indices_mu))
-    dr_mu = compute_dr_time_avg(coord_mu, res_indices_mu, window)
-    dr_wt_tmp = np.mean(dr_wt[res_indices_wt])
-    dr_mu_tmp = np.mean(dr_mu)
+        if ord('Z') >= ord(folder[i][-1]) >= ord('A'):
+            target_res_index = int(folder[i][1:-1]) - 1
+            res_to_target_res_distance_wt = np.sum((r_wt - r_wt[target_res_index])**2, axis = 1)**0.5
+            res_indices_wt = np.where(res_to_target_res_distance_wt <= res_cutoff)[0]
+            r_mu = compute_r_time_avg(coord_mu, 'all')
+            res_indices_mu = res_indices_wt
+            print(i, folder[i], list(res_indices_mu))
+        else:
+            target_res_index = int(folder[i][1:]) - 1
+            res_to_target_res_distance_wt = np.sum((r_wt - r_wt[target_res_index])**2, axis = 1)**0.5
+            res_indices_wt = np.where(res_to_target_res_distance_wt <= res_cutoff)[0]
+            res_indices_wt = np.delete(res_indices_wt, np.where(res_indices_wt == target_res_index)[0])
+            res_indices_mu = np.where(res_indices_wt > target_res_index, res_indices_wt-1, res_indices_wt)
+            print(i, folder[i], list(res_indices_wt), list(res_indices_mu))
+        
+        if mass_weight_fg:
+            res_mass = []
+            for res_index in res_indices_mu: res_mass.append(res_mass_map[element_mu[res_index]])
+            res_mass = np.array(res_mass)
+        else:
+            res_mass = np.ones(len(res_indices_mu))
+        dr_mu = compute_dr_time_avg(coord_mu, res_indices_mu, window)
+        dr_wt_tmp = np.average(dr_wt[res_indices_wt], weights = res_mass)
+        dr_mu_tmp = np.average(dr_mu, weights = res_mass)
+
     fout.write('%8s %3.6e %3.6e %3.6e %6s\n'%(folder[i], dr_mu_tmp, dr_wt_tmp, dr_mu_tmp/dr_wt_tmp, len(res_indices_mu)))
 fout.close()
--- compute-rank-res-ratio-3d.py
+++ compute-rank-res-ratio-3d.py
@@ -2,23 +2,27 @@ import os
 import sys
 import numpy as np
 
-res_cutoff = int(sys.argv[1])
+sufix = ""
+mass_weight_fg = False
+res_cutoff = float(sys.argv[1])
+if '-mass' in sys.argv[2:]:
+    mass_weight_fg = True
+    sufix += '-massweighted'
 
 seq_id_mutation_map = {}
 with open("single-mutation-map.txt","r") as fin:
     for aline in fin:
         linelist = aline.strip().split()
         seq_id_mutation_map[int(linelist[1])] = linelist[0]
-
 mutation_msd_map = {}
-with open("res-com-msd-ratio-range-3d=%.2f.txt"%(res_cutoff),"r") as fin:
+with open("res-com-msd-ratio-range-3d=%.2f%s.txt"%(res_cutoff, sufix),"r") as fin:
     fin.readline()
     for aline in fin:
         linelist = aline.strip().split()
         mutation_msd_map[linelist[0]] = np.array([float(i) for i in linelist[1:]])
 
 seq_id_list = [i for i in range(31390,33802+1)]
-foutrescom = open("submission-res-com-range-3d=%.2f.csv"%(res_cutoff),"w")
+foutrescom = open("submission-res-com-range-3d=%.2f%s.csv"%(res_cutoff, sufix),"w")
 foutrescom.write("seq_id,tm\n")
 for seq_id in seq_id_list:
     mutation = seq_id_mutation_map[seq_id]
